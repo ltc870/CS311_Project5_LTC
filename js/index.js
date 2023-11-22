b9eef76d8425dc543a2ff958747a263b236b7232
Lawrence C <lawrencecaudle870@gmail.com>
2023-11-22 12:55:20 -0500

    added comments to code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 "use strict";
+
+// resetButton, formContainer, formButton variables
 const resetButton = document.createElement("button");
 const formContainer = document.getElementById("form-container");
 const formButton = document.getElementById("button");
 
+// generateMadLib function
 const generateMadLib = function () {
+  // madLib variables for each input
   const restaurantName = document.getElementById("word1").value;
   const adjective = document.getElementById("word2").value;
   const adjective2 = document.getElementById("word3").value;
@@ -20,19 +24,26 @@ const generateMadLib = function () {
   const noun2 = document.getElementById("word14").value;
   const adjective6 = document.getElementById("word15").value;
 
+  // madLib variable that contains the mad lib
   const madLib = `Last night I went to <span style="color: blue">${restaurantName}</span> for dinner. The food was <span style="color: blue">${adjective}</span> and <span style="color: blue">${adjective2}</span>, but I enjoyed the <span style="color: blue">${noun}</span>. Our waitress was kind of <span style="color: blue">${adjective3}</span> because she kept on <span style="color: blue">${verb}</span> and she got our order <span style="color: blue">${adjective4}</span>. She also dropped <span style="color: blue">${food}</span> on my mom's <span style="color: blue">${bodyPart}</span>. My mom was really <span style="color: blue">${adjective5}</span> because it ruined her <span style="color: blue">${clothing}</span>. For dessert, we got a <span style="color: blue">${dessert}</span> that had <span style="color: blue">${color}</span> frosting. It was pretty good but when I was chewing, I found a <span style="color: blue">${noun2}</span> in the middle. All in all, it was a <span style="color: blue">${adjective6}</span> experience.`;
 
+  // formContainer innerHTML to change to madLib
   formContainer.innerHTML = ` <div class="madlib">${madLib}</div>`;
+
+  // appends the button to the formContainer
   formContainer.appendChild(resetButton);
 
+  // resetButton innerHTML and classList
   resetButton.innerHTML = "Reset";
   resetButton.classList.add("reset-button");
 };
 
+// resetButton event listener
 resetButton.addEventListener("click", function () {
   location.reload();
 });
 
+// formButton event listener
 formButton.addEventListener("click", function (e) {
   e.preventDefault();
   generateMadLib();
